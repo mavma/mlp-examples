3be82d15d7be5eeb9053c2aba9b1d438da238e0a
mavma <33549598+mavma@users.noreply.github.com>
2020-01-05 16:41:20 +0100

    Call C functions from F using pointers set by C

--- csrc.c
+++ csrc.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int helloworldc() {
-    printf("Hello, World from C!\n");
-    return 0;
+void myfunc(float a) {
+    printf("Called with float %f\n", a);
+}
+
+void setFunc(void* f);
+void callFunc();
+
+int main( int argc, const char* argv[] )
+{
+    void* f = (void*) &myfunc;
+    setFunc(f);
+    callFunc();
 }
--- fsrc.f90
+++ fsrc.f90
@@ -1,9 +1,30 @@
-program hello
-    use, intrinsic ::  iso_c_binding
+! adapted from http://fortranwiki.org/fortran/show/c_f_procpointer
+
+module use_fptr
+    use iso_c_binding
+    implicit none
+
     interface
-        subroutine hwc() bind(C, name='helloworldc')
+        subroutine func(a)
+            import :: c_float
+            real(c_float), intent(in) :: a
         end subroutine
     end interface
 
-   call hwc()
-end program hello
+    procedure(func), pointer :: myCFunc
+
+contains
+    subroutine setFunc(f) bind(c, name="setFunc")
+        use iso_c_binding
+        type(c_funptr), value :: f
+
+        call c_f_procpointer(f, myCFunc)
+    end subroutine setFunc
+
+    subroutine callFunc() bind(c, name='callFunc')
+        real(c_float) :: a
+        a = 3.0
+        call myCFunc(a)
+        call myCFunc(3.0) ! outputs 0, why?
+    end subroutine callFunc
+end module use_fptr
